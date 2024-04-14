79a50797ae2ae6563327a40f45f9105b9210b431
Matverk <matverik0@gmail.com>
2024-04-14 13:29:46 +0300

    file reading

--- src/file_In_out.c
+++ src/file_In_out.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include "types.h"
+#include "file_In_out.h"
 
-void reading_from_file(FILE* fp, symbol* simbols, int* kolvo, int* kk, int* k) {
-
-    // :)
+void reading_from_file(FILE* fp, symbol* simbols, int* kolvo, int* allk, int* uniqk) {
+    unsigned char tmpc;
+    while ((tmpc = fgetc(fp)) && !feof(fp)) {
+        ++(*allk);
+        int is_new = 1;
+        int i;
+        for (i = 0; i < *uniqk; ++i) {
+            if (tmpc == simbols[i].ch) {
+                is_new = 0;
+                break;
+            }
+        }
+        if (is_new) {
+            symbol s;
+            s.ch = tmpc;
+            simbols[*uniqk] = s;
+            kolvo[*uniqk] = 1;
+            ++(*uniqk);
+        }
+        else ++(kolvo[i]);
+    }
 }
 
 void writing_to_file(FILE* fp2, FILE* fp3, symbol* simbols, int* kolvo, int* kk, int* k, int* fsize2) {
--- src/file_In_out.h
+++ src/file_In_out.h
@@ -1,6 +1,14 @@
 #ifndef FILE_H3
 #define FILE_H3
 
-void reading_from_file(FILE*, symbol*, int*, int*, int*);
+/// @brief Чтение и группировка символов
+/// @param fp входной бинарный файл
+/// @param simbols выходной массив уникальных символов
+/// @param kolvo выходной массив частот символов
+/// @param allk количество прочитанных символов
+/// @param uniqk количество уникальных символов
+void reading_from_file(FILE* fp, symbol* simbols, int* kolvo, int* allk, int* uniqk);
+
+
 void writing_to_file(FILE*, FILE*, symbol*, int*, int*, int*, int*);
 #endif
