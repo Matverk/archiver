a9a78a8009d21065771cfd34dcfcb1bd8fe3ee10
Matverk <matverik0@gmail.com>
2024-04-15 18:47:17 +0300

    add symbols info

--- src/information.c
+++ src/information.c
@@ -4,8 +4,8 @@
 
 void show_information(symbol* simbols, int* uniqk, int* allk, int* fsize_2) {
     for (int i = 0; i < *uniqk; i++)
-        printf("N: %i\tCode: %d\tFreq: %f\tQuant: %.0f\tSymb: \"%c\"\n",
-            i, simbols[i].ch, simbols[i].freq, simbols[i].freq * (*allk), simbols[i].ch);
+        printf("N: %i\tCode: %d\tFreq: %f\tQuant: %.0f\tSymb: \"%c\"\tCompressed: %s\n",
+            i, simbols[i].ch, simbols[i].freq, simbols[i].freq * (*allk), simbols[i].ch, simbols[i].code);
     printf("\nLetters: %d\n", *allk);
     printf("Entropy: %f\n", entropy_count(simbols, uniqk));
     printf("Bit costs: %f\n", bit_costs(fsize_2, *allk));
