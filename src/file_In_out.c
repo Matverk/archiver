ab9b4006c0b9c9b4a3cb27fbed46e66e9a91feee
Matverk <matverik0@gmail.com>
2024-04-15 18:48:07 +0300

    file writing

--- src/file_In_out.c
+++ src/file_In_out.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "types.h"
 #include "file_In_out.h"
 
 void reading_from_file(FILE* fp, symbol* simbols, int* kolvo, int* allk, int* uniqk) {
@@ -17,6 +16,9 @@ void reading_from_file(FILE* fp, symbol* simbols, int* kolvo, int* allk, int* un
         if (is_new) {
             symbol s;
             s.ch = tmpc;
+            sprintf(s.code, "");
+            s.left = NULL;
+            s.right = NULL;
             simbols[*uniqk] = s;
             kolvo[*uniqk] = 1;
             ++(*uniqk);
@@ -25,8 +27,46 @@ void reading_from_file(FILE* fp, symbol* simbols, int* kolvo, int* allk, int* un
     }
 }
 
-void writing_to_file(FILE* fp2, FILE* fp3, symbol* simbols, int* kolvo, int* kk, int* k, int* fsize2) {
-
-    // :)
+void writing_to_file(FILE* fp2, FILE* fp3, int* fsize2) {
+    char ch;
+    int i = 1;
+    union code code1;
+    while ((ch = fgetc(fp2)) != EOF) {
+        ++(*fsize2);
+        if (i > 8) {
+            i = 1;
+            fputc(code1.sym_to_write, fp3);
+            code1.sym_to_write = '\0';
+        }
+        switch (i) {
+        case 1:
+            code1.byte.b1 = ch - 48;    // 48 - код символа 0
+            break;
+        case 2:
+            code1.byte.b2 = ch - 48;
+            break;
+        case 3:
+            code1.byte.b3 = ch - 48;
+            break;
+        case 4:
+            code1.byte.b4 = ch - 48;
+            break;
+        case 5:
+            code1.byte.b5 = ch - 48;
+            break;
+        case 6:
+            code1.byte.b6 = ch - 48;
+            break;
+        case 7:
+            code1.byte.b7 = ch - 48;
+            break;
+        case 8:
+            code1.byte.b8 = ch - 48;
+            break;
+        default:
+            break;
+        }
+        ++i;
+    }
+    fputc(code1.sym_to_write, fp3);
 }
-
